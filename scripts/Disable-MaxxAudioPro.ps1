20210abff0990cd44c6cb0828944a4bc7ac7c7bf
hxr404 <55095883+hxr404@users.noreply.github.com>
2021-03-22 18:21:12 +0100

    Create Disable-MaxxAudioPro.ps1

---
+++ scripts/Disable-MaxxAudioPro.ps1
@@ -0,0 +1,26 @@
+# Credit to https://github.com/W4RH4WK/Debloat-Windows-10/blob/a3c386ca5a965a844b8a4842ff5e8f1e8807098a/scripts/Disable-MaxxAudioPro.ps1
+
+
+Disable-ScheduledTask RtHDVBg_PushButton -ErrorAction SilentlyContinue | Out-Null
+
+# Realtek Audio Service
+Stop-Service RtkAudioService
+Set-Service RtkAudioService -StartupType Disabled
+
+# Waves Audio Services
+Stop-Service WavesSysSvc
+Set-Service WavesSysSvc -StartupType Disabled
+
+$regFileName = "Disable-MaxxAudioPro.reg"
+$regFileContent = @"
+Windows Registry Editor Version 5.00
+[HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run]
+"RtHDVBg_PushButton"=hex:03,00,00,00,0b,34,17,77,53,4c,d3,01
+"RtHDVBg_WAVES_SKYLAKE"=hex(3):03,00,00,00,4F,89,38,1E,5D,A1,D2,01
+"RTHDVCPL"=hex(3):03,00,00,00,10,C0,D1,1F,5D,A1,D2,01
+"WavesSvc"=hex(3):03,00,00,00,F8,39,39,21,5D,A1,D2,01
+"@
+
+$regFileContent | Out-file $regFileName
+Start-Process -FilePath "regedit.exe" -ArgumentList "/s", """$regFileName""" -Wait
+Remove-Item $regFileName
